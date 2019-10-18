89cf035c19386cef85c4d329ebd85e063c28ea89
AlexsandrDowgaluk <Dowgaluk88@gmail.com>
2019-10-18 11:54:34 +0300

    Добавьте файлы проекта.

---
+++ Dev/ConnectFirefox.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+
+
+
+namespace Dev
+{
+    public class ConnectFirefox
+    {
+        public IWebDriver driver;
+
+        public ConnectFirefox()
+
+
+        {
+            Initialize();
+
+            driver.Navigate().GoToUrl("https://deveducation.com/");
+        }
+        public void Initialize()
+        {
+
+            FirefoxDriverService service = FirefoxDriverService.CreateDefaultService();
+            service.FirefoxBinaryPath = @"C:\Program Files\Mozilla Firefox\firefox.exe";
+            driver = new FirefoxDriver(service);
+
+
+        }
+    }
+}
---
+++ DevEducationTests/CytiTests.cs
@@ -0,0 +1,24 @@
+﻿using Dev;
+using OpenQA.Selenium;
+using System;
+using Xunit;
+
+
+namespace DevEducationTests
+{
+    public class CytiTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            ConnectFirefox con = new ConnectFirefox();
+
+            //Thread.Sleep(2000);
+            //con.driver.FindElement(By.XPath("xpath=//img[@alt='close img']")).Click();
+            con.driver.FindElement(By.CssSelector(".az-point")).Click();
+            string actual = con.driver.FindElement(By.XPath("//button[contains(.,'Баку')]")).Text;
+            con.driver.Quit();
+            Assert.Equal("Баку", actual);
+        }
+    }
+}
